ebe560ff3549dc55f16e077e890c50faa3ec1553
danielchang2002 <danielchang2002@gmail.com>
2020-11-26 22:59:27 -0600

    L System video

--- LSystemFractalTrees/system.py
+++ LSystemFractalTrees/system.py
@@ -1,49 +1,46 @@
-from typing import List
-from typing import Dict
 import pygame
-import math
 import sys
-from time import sleep
-# [x] Define LSystem
+import math
+# [x] Define L System
 # [x] Implement Sentence Generation
-# [] Implement drawing
+# [x] Implement drawing
+#   [x] Set up pygame
+#   [x] Get command line input
+#   [x] Implement draw function
 
-# Axiom: Initial String
-# Production Rules: Change the String Iteratively
-# Make geometric structures using the string
-
+# L Systems generate sentences, translate sentences into geometric structures
+# Axiom: initial string
+# produciton rules: generates longer strings 
+# translate into geo structures
 
 class LSystem():
-    def __init__(self, axiom: str, rules: Dict[str, str], start: tuple, length: int, theta: int, ratio: float):
+    def __init__(self, axiom, rules, dtheta, start, length, ratio):
         self.sentence = axiom
         self.rules = rules
-        self.startx = start[0]
-        self.starty = start[1]
-        self.x = self.startx
-        self.y = self.starty
-        self.length = length
-        self.dtheta = math.radians(theta)
         self.theta = math.pi / 2
-        self.positions = []
+        self.dtheta = dtheta
+        self.start = start
+        self.x, self.y = start
+        self.length = length
         self.ratio = ratio
+        self.positions = []
 
     def __str__(self):
         return self.sentence
 
     def generate(self):
+        self.x, self.y = self.start
+        self.theta = math.pi / 2
         self.length *= self.ratio
-        self.theta = math.pi / 2 
-        self.x = self.startx
-        self.y = self.starty
-        newSentence = ""
+        newStr = ""
         for char in self.sentence:
-            mapped = ""
+            mapped = char
             try:
                 mapped = self.rules[char]
             except:
-                mapped = char
-            newSentence += mapped
-        self.sentence = newSentence
+                pass
+            newStr += mapped
+        self.sentence = newStr
 
     def draw(self, screen):
         color = 0
@@ -52,39 +49,37 @@ class LSystem():
             if char == 'F' or char == 'G':
                 x2 = self.x - self.length * math.cos(self.theta)
                 y2 = self.y - self.length * math.sin(self.theta)
-                pygame.draw.line(screen, (255 - color, color, 125 + color / 2), (self.x, self.y), (x2, y2))
-                self.x = x2
-                self.y = y2
-            if char == '+':
+                pygame.draw.line(screen, (255 - color, color, 125 + dcolor / 2), (self.x, self.y), (x2, y2))
+                self.x, self.y = x2, y2
+            elif char == '+':
                 self.theta += self.dtheta
-            if char == '-':
+            elif char == '-':
                 self.theta -= self.dtheta
-            if char == '[':
+            elif char == '[':
                 self.positions.append({'x' : self.x, 'y' : self.y, 'theta' : self.theta})
-            if char == ']':
-                positions = self.positions.pop()
-                self.x = positions['x']
-                self.y = positions['y']
-                self.theta = positions['theta']
+            elif char == ']':
+                position = self.positions.pop()
+                self.x, self.y, self.theta = position['x'], position['y'], position['theta']
             color += dcolor
 
 def main():
-    systemFile = sys.argv[1]
-    sizeScreen = int(sys.argv[2]), int(sys.argv[3])
+    l_sys_text = sys.argv[1]
+    pygame.init()
+    size = int(sys.argv[2]), int(sys.argv[3])
     start = int(sys.argv[4]), int(sys.argv[5])
     length = int(sys.argv[6])
     ratio = float(sys.argv[7])
-    pygame.init()
-    screen = pygame.display.set_mode(sizeScreen)
     system = None
-    with open(systemFile, 'r') as f:
+    with open(l_sys_text) as f:
         axiom = f.readline()
         numRules = int(f.readline())
         rules = {}
         for i in range(numRules):
             rule = f.readline().split(' ')
             rules[rule[0]] = rule[1]
-        system = LSystem(axiom, rules, start, length, int(f.readline()), ratio)
+        dTheta = math.radians(int(f.readline()))
+        system = LSystem(axiom, rules, dTheta, start, length, ratio)
+    screen = pygame.display.set_mode(size)
     running = True
     while running:
         for event in pygame.event.get():
@@ -96,6 +91,5 @@ def main():
                 system.generate()
         pygame.display.flip()
     pygame.quit()
-        
-if __name__ == '__main__':
-    main()
+
+main()
